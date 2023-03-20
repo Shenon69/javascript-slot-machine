4fe04659485016db3dc2c6a4724e915cc7ab281b
Shenon69 <ask.tikki69@gmail.com>
2023-03-21 01:53:58 +0530

    feat: deposit and number of lines

---
+++ project.js
@@ -0,0 +1,38 @@
+// 1. deposit money
+// 2. number of lines to bet on
+// 3. collect the bet amount
+// 4. spin the slot machine
+// 5. check the user won
+// 6. give the looser thier winnings
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter the deposit amount: ");
+    const numberDpositAmount = parseFloat(depositAmount);
+
+    if (isNaN(depositAmount) || depositAmount <= 0) {
+      console.log("Invalid deposit amount! Try again.");
+    } else {
+      return numberDpositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on(1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(lines) || lines <= 0 || lines > 3) {
+      console.log("Invalid line number! Try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
